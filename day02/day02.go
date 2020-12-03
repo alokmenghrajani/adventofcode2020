165e023b2663e0ac5af943af9e99cac7f7a67176
Alok Menghrajani <alok@squareup.com>
2020-12-02 20:54:06 -0800

    ParseToStruct

--- day02/day02.go
+++ day02/day02.go
@@ -1,23 +1,28 @@
 package day02
 
 import (
+	"github.com/alokmenghrajani/adventofcode2020/utils"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 func Part1(input []string) int {
+	type line struct {
+		Min int
+		Max int
+		C   string
+		S   string
+	}
 	re := regexp.MustCompile(`(\d+)-(\d+) (.): (.*)`)
-	valid := 0
-	for _, l := range input {
-		match := re.FindStringSubmatch(l)
-		min, _ := strconv.Atoi(match[1])
-		max, _ := strconv.Atoi(match[2])
-		c := match[3]
-		s := match[4]
 
-		t := strings.Count(s, c)
-		if t >= min && t <= max {
+	valid := 0
+
+	for _, l := range input {
+		var match line
+		utils.ParseToStruct(re, l, &match)
+
+		t := strings.Count(match.S, match.C)
+		if t >= match.Min && t <= match.Max {
 			valid++
 		}
 	}
@@ -25,17 +30,21 @@ func Part1(input []string) int {
 }
 
 func Part2(input []string) int {
+	type line struct {
+		Offset1 int
+		Offset2 int
+		C       byte
+		S       string
+	}
 	re := regexp.MustCompile(`(\d+)-(\d+) (.): (.*)`)
+
 	valid := 0
 	for _, l := range input {
-		match := re.FindStringSubmatch(l)
-		offset1, _ := strconv.Atoi(match[1])
-		offset2, _ := strconv.Atoi(match[2])
-		c := match[3][0]
-		s := match[4]
+		var match line
+		utils.ParseToStruct(re, l, &match)
 
 		// golang doesn't have any xor, but != is a fine replacement.
-		if (s[offset1-1] == c) != (s[offset2-1] == c) {
+		if (match.S[match.Offset1-1] == match.C) != (match.S[match.Offset2-1] == match.C) {
 			valid++
 		}
 	}
--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -42,4 +44,51 @@ func StringsToInts(input []string) []int {
 		numbers[i] = n
 	}
 	return numbers
+}
+
+func ParseToStruct(re *regexp.Regexp, input string, target interface{}) {
+	m := re.FindStringSubmatch(input)
+	if m == nil {
+		return
+	}
+
+	var useOffset bool
+
+	for i, name := range re.SubexpNames() {
+		if i == 0 {
+			continue
+		}
+		var field reflect.Value
+		if name == "" {
+			// use offset
+			if i == 1 {
+				useOffset = true
+			} else if !useOffset {
+				panic("can't mix named and unnamed subexpressions")
+			}
+			field = reflect.ValueOf(target).Elem().Field(i - 1)
+		} else {
+			// use name
+			if i == 1 {
+				useOffset = false
+			} else if useOffset {
+				panic("can't mix named and unnamed subexpressions")
+			}
+			field = reflect.ValueOf(target).Elem().FieldByName(name)
+		}
+		if field.Kind() == reflect.String {
+			field.SetString(m[i])
+		} else if field.Kind() == reflect.Int {
+			v, err := strconv.Atoi(m[i])
+			PanicOnErr(err)
+			field.SetInt(int64(v))
+		} else if field.Kind() == reflect.Uint8 {
+			if len(m[i]) != 1 {
+				panic(fmt.Sprintf("expecting 1 char, got: %s", m[i]))
+			}
+			field.SetUint(uint64(m[i][0]))
+		} else {
+			panic(fmt.Sprintf("unknown kind: %s", field.Kind()))
+		}
+	}
 }
